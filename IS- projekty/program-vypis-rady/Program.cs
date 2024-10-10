5c07ee2a363b54220564070f7a37f6b606daf81b
Lucie Stejskalová <lucie.stejskalova@tul.cz>
2024-10-10 13:34:32 +0200

    #001

---
+++ IS- projekty/program-vypis-rady/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+
+    static void Main() {
+        //chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+                Console.Clear();
+                Console.WriteLine("********************");
+                Console.WriteLine("*********Výpis řady***********");
+                Console.WriteLine("********************");
+                Console.WriteLine("**********Lucie Stejskalová**********");
+                Console.WriteLine("********************");
+                Console.WriteLine("*****************\n\n");
+                Console.WriteLine();
+        // Vstup do uživatle - TO DO
+
+        // Logika pro výpis řady- TO DO
+
+        // Opakování programu
+        Console. WriteLine ("Program opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+            }
+          }      
+
+
+
+
+}
