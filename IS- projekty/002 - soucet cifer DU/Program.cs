f6537309074bc96aa0c5c73fc6a391e85371ec66
Lucie Stejskalová <lucie.stejskalova1@tul.cz>
2024-10-23 18:56:02 +0200

    #2 cifry

---
+++ IS- projekty/002 - soucet cifer DU/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace soucetcifer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Napiš číslo : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = soucetcisel(number);
+            Console.WriteLine($"Součet čísel je: {sum}");
+        }
+
+        static int soucetcisel(int number)
+        {
+            if (number != 0)
+            {
+                return (number % 10 + soucetcisel(number / 10));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ IS- projekty/program 000/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+
+    static void Main() {
+        //chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+                Console.Clear();
+                Console.WriteLine("********************");
+                Console.WriteLine("*********Součet a součin cifer***********");
+                Console.WriteLine("********************");
+                Console.WriteLine("**********Lucie Stejskalová**********");
+                Console.WriteLine("********************");
+                Console.WriteLine("*****************\n\n");
+                Console.WriteLine();
+        // Vstup od uživatle - TO DO
+
+    
+
+        // Vstup od uživatele - lepší varianta
+        Console.Write("Zadejte první číslo řady (celé číslo):");
+        int number;
+        while(!int.TryParse(Console.ReadLine(), out number)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo):");
+        }
+
+        //Výpis vstupní hodnoty 
+        Console.WriteLine ("================");
+        Console.WriteLine ("Uživatel zadal: {0}", number);
+        Console.WriteLine ("================"\n\n);
+
+        int suma = 0;
+        int numberBackup = number;
+        int digit;
+
+        //Pokud je číslo záporné tak minus odstraníme
+        if (number < 0){
+            number = - number;
+        }
+        
+        
+        while(number >= 10) {
+            digit = number % 10; //% opearátor mod
+            number = (number - digit) / 10;
+            Console.WriteLine("Digit = {0}", digit);
+            Console.WriteLine("Digit = {0}, number");
+            suma = suma + digit;
+        }
+        Console.WriteLine("Digit = {0}, number");
+        suma = suma + number;
+        
+        
+        Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+        
+        
+        
+        
+        
+        
+      
+
+
+
+        // Opakování programu
+        Console. WriteLine ("Program opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+            }
+          }      
