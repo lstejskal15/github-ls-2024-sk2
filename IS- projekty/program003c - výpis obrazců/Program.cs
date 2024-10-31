c8dae7ab777d2617bb76767e883555e1b5253399
Lucie Stejskalová <lucie.stejskalova1@tul.cz>
2024-10-31 15:47:46 +0100

    #3 DU

---
+++ IS- projekty/program003c - výpis obrazců/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int n = 8;
+bool[,] array = new bool[n,n];
+
+for(int y = 0; y<n;y++){
+    for(int x = 0; x<n;x++){
+        if(x == 0 || x == n/2 || x+1 == n)
+        {
+            array [x,y] = true;
+            
+        }
+        if(y == 0 || y == n/2 || y+1 == n)
+        {
+            array [x,y] = true;
+            
+        }
+    }
+}
+
+for(int y = 0; y<n;y++){
+    for(int x = 0; x < n; x++){
+       if(array[x,y] == true){
+        Console.Write("* ");
+       }
+       else{
+        Console.Write("  ");
+        }
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
